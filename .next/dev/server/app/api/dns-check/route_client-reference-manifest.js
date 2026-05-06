a671e0a8b993ec32e098b41cffe3242ca724080e
moltresIn <akishor2001@gmail.com>
2026-05-06 06:26:31 +0530

    chore: remove .next from tracking

--- .next/dev/server/app/api/dns-check/route_client-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/api/dns-check/route"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/workspaces/dns-checker/app/api/dns-check/route":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}};
